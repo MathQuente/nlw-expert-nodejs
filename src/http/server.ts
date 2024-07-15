0378acbc801e15c11ff73c743396022b61d4c668
Matheus de Sousa <math.de.sousa.silva@gmail.com>
2024-07-15 12:07:15 -0300

    first commit

---
+++ src/http/server.ts
@@ -0,0 +1,26 @@
+import fastify from 'fastify'
+import cookie from '@fastify/cookie'
+import websocket from '@fastify/websocket'
+import { createPoll } from './routes/createPoll'
+import { getPoll } from './routes/getPoll'
+import { voteOnPoll } from './routes/voteOnPoll'
+import { pollResults } from './ws/pollResults'
+
+const app = fastify()
+
+app.register(cookie , {
+  secret: "polls-app-nwl", 
+  hook: 'onRequest'
+})
+
+app.register(websocket)
+
+app.register(createPoll)
+app.register(getPoll)
+app.register(voteOnPoll)
+
+app.register(pollResults)
+
+app.listen({port: 3333}).then(() => {
+  console.log("HTTP server running!")
+})
